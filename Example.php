191ad885f748b46a0becb3002509f0c6ab4672dc
PhucedMODZ <fardy662@gmail.com>
2019-12-14 21:52:08 +0000

    Cleaned Code

--- Example.php
+++ Example.php
@@ -1,9 +1,5 @@
 <?php
-
 include('XboxOneThemeLib.php');
-
 $theme = new XboxOneTheme();
-$real_ip = $theme->get_client_ip();
-$theme->get_client_color_by_ip($real_ip);
-
+$theme->get_client_color_by_ip($theme->get_client_ip());
 ?>
